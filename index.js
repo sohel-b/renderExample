32f3a28a667d5fbd292b0353f57a32fa7c878942
sohel-b <beparisohel2001@gmail.com>
2025-02-17 12:30:44 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.get("/", (req, res) => {
+    res.send("This is my First Deployed Project")
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`)
+})
+
+
